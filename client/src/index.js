1dddc12d089d50b9cee48448421f35692bea4d0d
Tarekul Islam <tarek.webappick@gmail.com>
2024-05-02 15:07:21 +0600

    Favicon Added

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import App from './App';
-import './index.css';
-import store from './store/store';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import App from "./App";
+import "./index.css";
+import store from "./store/store";
 
-const root = createRoot(document.getElementById('mindtoheart'));
+const root = createRoot(document.getElementById("wordcampsylhet"));
 root.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
