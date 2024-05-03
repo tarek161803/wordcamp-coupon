7a68c01779e43c7c13ed5f160a8430eb035f36d0
Tarekul Islam <tarek.webappick@gmail.com>
2024-05-03 15:04:44 +0600

    Email Header Image added

--- server/utils/mail.js
+++ server/utils/mail.js
@@ -21,14 +21,14 @@ transporter.verify(function (error, success) {
   }
 });
 
-const sendMail = async (email, coupon) => {
+const sendMail = async (user) => {
   options = {
     from: {
       name: "WebAppick",
       address: "hr@webappick.com",
     },
-    to: email,
-    subject: "Wordcamp 2024",
+    to: user.email,
+    subject: "Congrats on Winning the Swag! Bonus: You Can Win An iPad too.",
     text: "",
     html: `<!DOCTYPE html>
 <html>
@@ -37,34 +37,38 @@ const sendMail = async (email, coupon) => {
   </head>
   <body style="font-family: 'Inter', sans-serif">
     <div style="display: block; margin: auto; max-width: 600px" class="main">
-      <div></div>
-      <div>
-        <p>Hey there, Tarekul Islam</p>
-        <p style="margin-top: 32px">
-          You did it! Not only have you
-          <span style="color: #0a8adf; font-weight: 700">Rocked WordCamp Sylhet (2024),</span> but also snagged some
-          sweet swag by scanning our QR code - double win! We're thrilled to have you on board and appreciate your
-          enthusiasm.
-        </p>
-        <p style="color: #ff485e; font-weight: 700; margin-top: 32px">
-          You've won a ____. Please collect your Swag from Webappick Stall
-        </p>
-        <p style="margin-top: 32px">
-          But that's not all! As a token of our appreciation, we've included
-          <span style="color: #ff485e; font-weight: 700">a special coupon</span> for the raffle draw
-          <span style="color: #0a8adf; font-weight: 700">later today at 4 PM.</span> Who knows, you might be the lucky
-          winner of an awesome iPad too!
-        </p>
-        <p style="margin-top: 32px">Here's your Coupon —</p>
-        <p style="margin-top: 32px">
-          We had a blast connecting with you and are excited to see what you create next! Stay tuned for more updates
-          and keep an eye out for that swag reveal. See you at the next WordCamp!
-        </p>
-        <p style="margin-top: 32px; margin-bottom: 6px">See you online</p>
-        <p style="font-weight: 700; font-style: italic; margin: 0px">WebAppick</p>
+     <div style="width: 100%">
+        <img style="width: 100%" src="https://www.webappick.info/image/email_header.png" alt="Header Banner" />
       </div>
       <div>
-        <p>WebAppick - Unlimited Business Booster Plugin</p>
+        <p>Hey there, ${user.name}</p>
+        <p style="margin-top: 24px">
+          You Did it. Not only you have Rocked the WordCamp Sylhet (2024), but also got some cool swag.
+        </p>
+        <p style="color: #dd40b3; font-weight: 700; margin-top: 24px; margin-bottom: 6px">You've won a <span style="text-transform: capitalize;"> ${user.swag}</span></p>
+        <p style="margin-top: 0">Collect your Swag from Our Stall.</p>
+        <p style="margin-top: 24px">
+          But that's not all! Use this coupon below for a chance to
+          <span style="color: #0088f7; font-weight: 700">Win an iPad</span> at our bonus raffle draw
+          <span style="color: #dd40b3; font-weight: 700">Today at 4 PM!</span>
+        </p>
+        <p style="margin-top: 24px; color: #0088f7; font-weight: 700">Coupon Code - ${user.coupon}</p>
+        <p style="margin-top: 24px">
+          We loved meeting you. Stay tuned for more updates and the cool swag reveal. See you at the next WordCamp,
+          champ!
+        </p>
+        <p style="margin-top: 24px; margin-bottom: 6px">See you online</p>
+        <p style="font-style: italic; margin: 0px">Webappick</p>
+      </div>
+      <div style="border-top: 1px solid #e0e3e9; margin-top: 24px">
+        <p style="text-align: center; font-weight: 600; margin-bottom: 8px">WEBAPPICK</p>
+        <div style="display: flex; justify-content: center">
+          <div style="border-top: 1px solid #6e7a91; width: 10px"></div>
+        </div>
+        <p style="text-align: center; font-size: 10px; color: #6e7a91">
+          Growth Partner for WooCommerce <br />
+          Business Across the Globe
+        </p>
       </div>
     </div>
 
@@ -80,6 +84,7 @@ const sendMail = async (email, coupon) => {
     </style>
   </body>
 </html>
+
 `,
   };
 
