7ce8a052cb573b252cb36514d8dd8ab8ade44a37
Tarekul Islam <tarek.webappick@gmail.com>
2024-05-09 18:14:53 +0600

    No Gift Available Message Added

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const path = require("path");
 
 const app = express();
-app.use(cors({ origin: "https://www.webappick.info" }));
+app.use(cors());
 app.use(express.static("static"));
 app.use(express.json());
 
